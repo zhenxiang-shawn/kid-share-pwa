0daee8af6e3c1c81c4d61493b776a9eacac1e6f1
zhenxiang-shawn <zhenxiang.shawn@outlook.com>
2024-05-16 16:49:02 +0800

    remove image tab

--- src/layout/bottom-tabbar/index.vue
+++ src/layout/bottom-tabbar/index.vue
@@ -11,9 +11,9 @@ const onChange = (index: number) => {
   <div>
     <van-tabbar v-model="active" @change="onChange">
       <van-tabbar-item replace to="/home" icon="home-o">首页</van-tabbar-item>
-      <van-tabbar-item replace to="/photos" icon="photo-o">
+      <!-- <van-tabbar-item replace to="/photos" icon="photo-o">
         照片
-      </van-tabbar-item>
+      </van-tabbar-item> -->
       <!-- <van-tabbar-item icon="friends-o">标签</van-tabbar-item> -->
       <van-tabbar-item replace to="/my" icon="setting-o">设置</van-tabbar-item>
     </van-tabbar>
